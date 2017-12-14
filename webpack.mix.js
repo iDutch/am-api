e19d44d87fe22c9661e38402956c3e9af8add47b
Richard Hoogstraaten <richard@codebridge.nl>
2017-12-14 12:25:49 +0100

    Fix whitespace

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,3 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .extract(['axios', 'bootstrap-slider', 'autobahn'], 'public/js/vendor.js')
    .sass('resources/assets/sass/app.scss', 'public/css');
-
