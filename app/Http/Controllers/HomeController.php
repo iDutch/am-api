a8fc4086e9e5cccf60bdb7e8da5b84921c9efc48
Richard Hoogstraaten <richard@hoogstraaten.eu>
2017-12-28 14:17:06 +0100

    ...

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,11 +27,9 @@ class HomeController extends Controller
         $HttpBridge = new HttpBridge();
         $Caller = $HttpBridge->createCaller('https', 'cb.hoogstraaten.eu', 443, '/call', config('app.crossbar_http_bridge_caller_key'), config('app.crossbar_http_bridge_caller_secret'), null, false);
 
-        $session = $Caller->call('wamp.subscription.lookup', ['eu.hoogstraaten.fishtank.publish']);
-
-
-        //$Caller->call('wamp.subscription.list_subscribers', []);
-        var_dump((string) $session['args'][0]);
+        $subscription = $Caller->call('wamp.subscription.lookup', ['eu.hoogstraaten.fishtank.publish']);
+        $subscribers = $Caller->call('wamp.subscription.list_subscribers', [$subscription['args'][0]]);
+        var_dump($subscribers);
 
         return view('home');
     }
