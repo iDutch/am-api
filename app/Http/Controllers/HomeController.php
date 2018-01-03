e2a4d7f4cb8b16e3920e84e1d2aa42c5a7e2be59
Richard Hoogstraaten <richard@hoogstraaten.eu>
2018-01-03 01:37:48 +0100

    Fix error when no clients are connected to crossbar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,14 +32,14 @@ class HomeController extends Controller
         $Caller = $HttpBridge->createCaller('https', 'cb.hoogstraaten.eu', 443, '/call', config('app.crossbar_http_bridge_caller_key'), config('app.crossbar_http_bridge_caller_secret'), null, false);
 
         $subscription = $Caller->call('wamp.subscription.lookup', ['eu.hoogstraaten.fishtank.publish']);
-        $subscribers = $Caller->call('wamp.subscription.list_subscribers', [$subscription['args'][0]]);
-
         $clients = [];
-        $i = 0;
-        foreach ($subscribers['args'] as $subscriber) {
-            $clients[$i] = $Caller->call('wamp.session.get', [$subscriber[0]])['args'][0];
-            $clients[$i]['active_schedule_id'] = $Caller->call('eu.hoogstraaten.fishtank.getactivescheduleid.'. $subscriber[0])['args'][0];
-            $i++;
+
+        if (!is_null($subscription['args'][0])) {
+            $subscribers = $Caller->call('wamp.subscription.list_subscribers', [$subscription['args'][0]]);
+            foreach ($subscribers['args'][0] as $key => $subscriber) {
+                $clients[$key] = $Caller->call('wamp.session.get', [$subscriber])['args'][0];
+                $clients[$key]['active_schedule_id'] = $Caller->call('eu.hoogstraaten.fishtank.getactivescheduleid.'. $subscriber)['args'][0];
+            }
         }
         return view('home', ['clients' => $clients, 'schedules' => $schedules]);
     }
